b0c4bdc8f8b6a0caa7b49601b1b74dde41a4e0fe
antoine <antoine.drionduchapoi@student.vinci.be>
2023-12-04 18:43:02 +0100

    coding input

--- api.py
+++ api.py
@@ -6,12 +6,12 @@ openai.api_key = OPENAI_API_KEY
 
 def chat_with_gpt(prompt):
     response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # Updated model name
+        model="gpt-3.5-turbo", 
         messages=[{"role": "system", "content": "You are a helpful assistant."}, {"role": "user", "content": prompt}]
     )
     return response['choices'][0]['message']['content'].strip()
 
-user_input = "What is the meaning of life?"
+user_input = input("What is your question for ChatGPT ? ")
 gpt_response = chat_with_gpt(user_input)
 print(gpt_response)
 
